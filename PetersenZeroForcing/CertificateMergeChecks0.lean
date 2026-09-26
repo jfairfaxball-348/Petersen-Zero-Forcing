f89d1bc32b4ae6c2f849604a76dfb62b2f45ee19
John Fairfax-Ball <jfairfaxball@gmail.com>
2026-09-26 13:56:56 +0100

    Raise Palomar merge-check recursion limit

--- PetersenZeroForcing/CertificateMergeChecks0.lean
+++ PetersenZeroForcing/CertificateMergeChecks0.lean
@@ -3,6 +3,8 @@ import PetersenZeroForcing.Certificate
 namespace PetersenZeroForcing
 namespace Certificate
 
+set_option maxRecDepth 1000000
+
 set_option maxHeartbeats 4000000 in
 theorem merge_group_0_checked :
     CertificateData.mergeGroup0.all mergeRowOKB = true := by decide
