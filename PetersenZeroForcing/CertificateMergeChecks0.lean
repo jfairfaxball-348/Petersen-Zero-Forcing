576a8317bba206e6c1471ce9320ff64065fba80e
John Fairfax-Ball <jfairfaxball@gmail.com>
2026-09-26 13:28:59 +0100

    Raise Palomar certificate recursion limit

--- PetersenZeroForcing/CertificateMergeChecks0.lean
+++ PetersenZeroForcing/CertificateMergeChecks0.lean
@@ -7,6 +7,7 @@ set_option maxHeartbeats 4000000 in
 theorem merge_group_0_checked :
     CertificateData.mergeGroup0.all mergeRowOKB = true := by decide
 
+set_option maxRecDepth 100000 in
 set_option maxHeartbeats 4000000 in
 theorem merge_group_1_checked :
     CertificateData.mergeGroup1.all mergeRowOKB = true := by decide
